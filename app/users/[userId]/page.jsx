43b4e987206c6b87b1670a98777fdcd51cf54541
Anatoly <boronilov@gmail.com>
2023-01-16 17:56:03 +0200

    static rendering added, tweet model created

--- app/users/[userId]/page.jsx
+++ app/users/[userId]/page.jsx
@@ -1,10 +1,19 @@
-import { getUserById } from "@/lib/prisma/users";
+import { getUserById, getUsers } from "@/lib/prisma/users";
 import React from "react";
 import User from "./user";
+
+export async function generateStaticParams() {
+  const {users} = await getUsers();
+
+  return users.map(({id}) => ({
+    userId: id
+  }))
+}
 
 const Page = async ({ params }) => {
   const { userId } = params;
   const { user } = await getUserById(userId);
+  
   return (
     <div>
       <User user={user} />
---
+++ app/users/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='my-6 mx-8'>
+        <h2 className='text-lg font-medium'>Loading...</h2>
+    </div>
+  )
+}
+
+export default Loading
