cfb807adb1351a42a78b2ed869953f40421ac275
Anatoly <boronilov@gmail.com>
2023-01-16 10:13:10 +0200

    basic structure, basic styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
-import './globals.css'
+import Footer from "./footer";
+import "./globals.css";
+import Header from "./header";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +10,14 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+
+      <body >
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Footer/>
+      </body>
     </html>
-  )
+  );
 }
